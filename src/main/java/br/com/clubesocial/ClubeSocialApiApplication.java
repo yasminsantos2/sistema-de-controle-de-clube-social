19a8e9a804f2db4b444027b79d749a5e1d5223b6
yasminsantos2 <yasminsantos.eng@gmail.com>
2026-04-16 20:43:06 -0300

    chore: inicializa projeto

---
+++ src/main/java/br/com/clubesocial/ClubeSocialApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.clubesocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubeSocialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubeSocialApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/clubesocial/ClubeSocialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clubesocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubeSocialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
